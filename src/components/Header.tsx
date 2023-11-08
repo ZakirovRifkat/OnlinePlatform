f2e08cb9ad8cb5f69a721f6e1275f34822169d95
Zakirov Rifkat <zakirov2323rifkat@gmail.com>
2023-11-09 00:22:51 +0300

    add: Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,48 @@
 import styled from "styled-components";
-import { Container } from "../container";
 
 export const Header = () => {
     return (
         <HeaderContainer>
-            <Container></Container>
+            <Container>
+                <HeaderImg src={"/image1.png"} />
+                <HeaderTitle>
+                    КАФЕДРА ПРИКЛАДНОЙ КИБЕРНЕТИКИ <br />
+                    Санкт-Петербургский государственный университет
+                    <br /> Математико-механический факультет
+                </HeaderTitle>
+                <HeaderImg src={"/image2.png"} />
+            </Container>
         </HeaderContainer>
     );
 };
 
 const HeaderContainer = styled.header`
     width: 100%;
-    height: 64px;
-    background: rgba(69, 69, 69, 0.9);
+    height: max-content;
+    padding: 10px 0;
+    background: #454545;
+`;
+
+const Container = styled.div`
+    width: var(--container);
+    max-width: var(--max-container);
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+const HeaderImg = styled.img`
+    height: 120px;
+    width: auto;
+`;
+const HeaderTitle = styled.div`
+    width: max-content;
+    color: #fff;
+
+    text-align: center;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 150%;
+    letter-spacing: 0.48px;
 `;
