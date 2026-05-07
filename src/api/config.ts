092f52bb3ed46f4ea71d905a25e6f705a50fdc90
Rifkat <zakirov2323rifkat@gmail.com>
2026-05-07 07:56:31 +0300

    fix: build #2

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL || "http://185.203.241.201";
+    import.meta.env.VITE_API_BASE_URL || "/api";
 
 export const API_TIMEOUT_MS = 15000;
